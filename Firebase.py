261f765ad63b721987bcbf6b2feaffdb1bc55829
KhohZongEu <khohzongeu@gmail.com>
2023-06-13 15:10:27 +0100

    Timer working

--- Firebase.py
+++ Firebase.py
@@ -21,19 +21,59 @@ db = firebase.database()
 print("Send Data to Firebase Using Raspberry Pi")
 print("----------------------------------------")
 print()
+this_millis = 0
+time_period = 1000
+sec = 0
+min = 0
+hour = 0
+occupancy = 1
+last_millis = 0
+
+
+def timeCount():
+  globals()["sec"] = globals()["sec"] +1
+  if globals()["sec"] > 60:
+    globals()["sec"] = 0
+    globals()["min"] = globals()["min"] + 1
+  if globals()["min"] > 60:
+    globals()["min"] = 0
+    globals()["hour"] = globals()["hour"] + 1  
+  print("Time: ")
+  print(globals()["sec"])  
 
 while True:
+  globals()["this_millis"] = int(time.time()*1000)#nanosec to millisec
+  print("Reading Firebase")
+  # occupancy_ref=db.child("data/occupancy/status").get()
+  # occupancy=occupancy_ref.val()
+  if occupancy == 1:
+    if globals()["this_millis"] - last_millis > time_period:
+      print("Triggered")
+      last_millis = globals()["this_millis"]
+      timeCount()
+      
+  print()
+  print(occupancy)
+  print()
+  print("All data collected")
+  print()
   
 
-  print("Updating Firebase")
+
+  print("Updating Firebase") 
   print()
 
   data = {
     "data/online": {
       "state":0
+    },
+    "data/occupancy":{
+      "status":0
+    },
+    "data/time":{
+      "minutes":globals()["min"],
+      "hours":globals()["hour"]
     }
   }
   db.update(data)
   print("Firebase Successfully Updated")
-
-  time.sleep(2)
